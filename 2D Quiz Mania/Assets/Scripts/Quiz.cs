925a3df22286b368d72d1252faf44ff0ea1d5adb
andreasnordqvist7 <148803484+andreasnordqvist7@users.noreply.github.com>
2024-09-07 13:39:48 +0200

    2D Quioz

--- 2D Quiz Mania/Assets/Scripts/Quiz.cs
+++ 2D Quiz Mania/Assets/Scripts/Quiz.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Sprite wrongAnswerSprite;
+
+
     void Start()
     {
         questionText.text = question.GetQuestion();
@@ -18,8 +25,19 @@ public class Quiz : MonoBehaviour
           buttonText.text = question.GetAnswer(i);
         }
 
-    }
 
+
+    }
+            public void OnAnswerSelected(int index)
+        {
+          if(index == question.GetCorrectAnswerIndex())
+          {
+            questionText.text = "Rätt svar!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+          }
+
+        }
 
 
 }
